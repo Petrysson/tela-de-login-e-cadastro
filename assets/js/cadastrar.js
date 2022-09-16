0290119261450b7ca4ba0e8745e71fc702249153
petrysson <petryssoncorreia@gmail.com>
2022-09-16 15:33:58 -0300

    funcionalidade ao clicar no botão cadastrar

--- assets/js/cadastrar.js
+++ assets/js/cadastrar.js
@@ -4,15 +4,23 @@ let eye_confirmationPassword = document.querySelector('#See-ConfirmePassword');/
 // VARIAVEIS DOS MEUS CAMPOS DO CADASTRO
 let name = document.querySelector('#name');
 let labelName = document.querySelector('#labelName');
+let validName = false;
 
 let user = document.querySelector('#user');
 let labelUser = document.querySelector('#labelUser');
+let validUser = false;
 
 let password = document.querySelector('#password');
 let labelPassword = document.querySelector('#labelPassword');
+let validPassword = false;
 
 let passwordConfirm = document.querySelector('#password-confirm');
 let label_ConfirmPassword = document.querySelector('#labelConfirmPassword');
+let validConfirmPassword = false;
+
+// VARIAVES DAS DIV QUE ANALISAM E VALIDAM POR COMPLETO OS CAMPOS APÓS O CLICK NO BTN "CADASTRAR"
+let msg_error = document.querySelector('#msg-error');
+let msg_sucess = document.querySelector('#msg-sucess');
 
 //VALIDAÇÕES DOS CAMPOS
 name.addEventListener('keyup', ()=>{
@@ -20,10 +28,12 @@ name.addEventListener('keyup', ()=>{
         labelName.setAttribute('style', 'color:red');
         labelName.innerHTML = ' <strong>NOME</strong> *INSIRA NO MINIMO 3 CARACTERES';
         name.setAttribute('style', 'border-color:red');
+        validName = false;
     } else{
         labelName.setAttribute('style', 'color:green');
         labelName.innerHTML = 'Nome';
         name.setAttribute('style', 'border-color:green');
+        validName = true;
     }
 })
 
@@ -32,10 +42,12 @@ user.addEventListener('keyup', ()=>{
         labelUser.setAttribute('style', 'color:red');
         labelUser.innerHTML = '<strong>USUARIO</strong> *Insira no minimo 4 caracteres';
         user.setAttribute('style', 'border-color:red');
+        validUser = false;
     } else{
         labelUser.setAttribute('style', 'color:green');
         labelUser.innerHTML = 'Usuario';
         user.setAttribute('style', 'border-color:green');
+        validUser = true;
     }
 })
 
@@ -44,27 +56,42 @@ password.addEventListener('keyup', ()=>{
         labelPassword.setAttribute('style', 'color:red');
         labelPassword.innerHTML = '<strong>SENHA</strong> *Insira no minimo 6 caracteres';
         password.setAttribute('style', 'border-color:red');
+        validPassword = false;
     } else{
         labelPassword.setAttribute('style', 'color:green');
         labelPassword.innerHTML = 'Senha';
         password.setAttribute('style', 'border-color:green');
+        validPassword = true;
     }
 })
 
 passwordConfirm.addEventListener('keyup', ()=>{
     if(password.value != passwordConfirm.value){
         label_ConfirmPassword.setAttribute('style', 'color:red');
-        label_ConfirmPassword.innerHTML = '<strong>CONFIRMAR SENHA</strong> *As senhas não são iguais';
+        label_ConfirmPassword.innerHTML = '<strong>CONFIRMAR SENHA</strong> *senhas não identicas';
         passwordConfirm.setAttribute('style', 'border-color:red');
+        validConfirmPassword = false;
     } else{
         label_ConfirmPassword.setAttribute('style', 'color:green');
         label_ConfirmPassword.innerHTML = 'Confirmar Senha';
         passwordConfirm.setAttribute('style', 'border-color:green');
+        validConfirmPassword = true;
     }
 })
 
 // FUNÇÃO DO BOTÃO CADASTRAR
 function cadastrar(){
+    if(validName && validUser && validPassword && validConfirmPassword){
+        msg_sucess.setAttribute('style', 'display:block');
+        msg_sucess.innerHTML = 'cadastrando...'
+        msg_error.innerHTML = '';
+        msg_error.setAttribute('style', 'display:none');
+    } else {
+        msg_error.setAttribute('style', 'display:block');
+        msg_error.innerHTML = 'preencha todos os campos corretamente';
+        msg_sucess.innerHTML = '';
+        msg_sucess.setAttribute('style', 'display:none');
+    }
 }
 
 // FAZER O ICONE DO OLHINHO MOSTRAR E ESCONDER A SENHA
