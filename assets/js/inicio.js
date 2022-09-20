6569b7522de592f988150246a2b0119d4f11fd64
petrysson <petryssoncorreia@gmail.com>
2022-09-20 11:53:27 -0300

    adicionei uma autenticação da minha tela de login, depois criei uma pasta para uma tela de inicio que só é possível acessa-lo após o usuário estiver cadastrado e logado no meu banco de dados do localStorage

---
+++ assets/js/inicio.js
@@ -0,0 +1,19 @@
+// MOSTRAR O NOME DO USUÁRIO LOGADO NA TELA
+// CRIAR UMA FUNÇÃO QUE AO CLICAR NO BTN "SAIR" ELE APAGUE A CHAVE TOKEN GERADA E VOLTE PARA A TELA DE LOGIN
+// DEPOIS CRIE UMA CONDIÇÃO QUE SÓ ACEITE A ENTRADA DE UM USUÁRIO NA TELA DE INICIO SE O SISTEMA TIVER GERADO UMA CHAVE TOKEN
+
+let userLogado = JSON.parse(localStorage.getItem('userLogado'));
+
+let logado = document.querySelector('#logado');
+logado.innerHTML = `Olá ${userLogado.user}, seja bem vindo`
+
+if(localStorage.getItem('token') == null){
+    alert('você precisa estar logado para ter acesso a essa página')
+    window.location.href = 'http://127.0.0.1:5500/login.html';
+
+}
+
+function go_out(){
+    localStorage.removeItem('token');
+    window.location.href = 'http://127.0.0.1:5500/login.html';
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -4,8 +4,60 @@ btn_eye.addEventListener('click', ()=>{
     let inputPassword = document.querySelector('#password');
 
     if(inputPassword.getAttribute('type') == 'password'){
-        inputPassword.setAttribute('type', 'text')
+        inputPassword.setAttribute('type', 'text');
     } else {
-        inputPassword.setAttribute('type', 'password')
+        inputPassword.setAttribute('type', 'password');
     }
 })
+
+function login(){
+    let user = document.querySelector('#user');
+    let labelUser = document.querySelector('#labelUser');
+
+    let password = document.querySelector('#password');
+    let labelPassword = document.querySelector('#labelPassword');
+
+    let msgError = document.querySelector('#msg-error');
+    let listUser = []
+
+    let userValid = {
+        name: '',
+        user: '',
+        password: ''
+    }
+
+    listUser =  JSON.parse(localStorage.getItem('listUser'));
+    
+    listUser.forEach((item) => {
+        if(user.value == item.user_CAD && password.value == item.password_CAD){
+            userValid = {
+                name: item.name_CAD,
+                user: item.user_CAD,
+                password: item.password_CAD
+            }
+        }
+    });
+
+    
+    if(user.value == userValid.user && password.value == userValid.password){
+        window.location.href = 'http://127.0.0.1:5500/assets/telaInicio/inicio.html';
+
+        // FAZER UMA CHAVE TOKEN PARA TER ACESSO A TELA DE INICIO E TER MAIS SEGURANÇA
+        let token = Math.random().toString(16).substring(2)
+        localStorage.setItem('token', token)
+
+        // INSERIR NOME DO USUARIO NA TELA DE INICIO
+        localStorage.setItem('userLogado', JSON.stringify(userValid));
+        
+    } else {
+        user.setAttribute('style', 'border-color:red');
+        labelUser.setAttribute('style', 'color:red');
+
+        password.setAttribute('style', 'border-color:red');
+        labelPassword.setAttribute('style', 'color:red');
+
+        msgError.setAttribute('style', 'display:block');
+        msgError.innerHTML = 'Usuário ou Senha incorretos';
+        user.focus();
+    }
+}
